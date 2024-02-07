ef0dc777227e150c64a9144830bd08bea6179103
muhsinachipra <muhsinachipra@gmail.com>
2024-02-07 21:13:04 +0530

    validation of BST

--- tree/binarySearchTree.js
+++ tree/binarySearchTree.js
@@ -155,6 +155,69 @@ class BinarySearchTree {
         }
         return root;
     }
+
+    sumOfAllElements(root) {
+        if (!root) {
+            return 0
+        } else {
+            let leftSum = this.sumOfAllElements(root.left)
+            let rightSum = this.sumOfAllElements(root.right)
+            return root.value + leftSum + rightSum
+        }
+    }
+
+    maxValue(root) {
+        if (!root) {
+            return Number.NEGATIVE_INFINITY
+        } else {
+            let leftMax = this.maxValue(root.left)
+            let rightMax = this.maxValue(root.right)
+            return Math.max(root.value, leftMax, rightMax)
+        }
+    }
+
+    heightOfTree(root) {
+        if (!root) {
+            return 0
+        } else {
+            let leftHeight = this.heightOfTree(root.left)
+            let rightHeight = this.heightOfTree(root.right)
+            return 1 + Math.max(leftHeight, rightHeight)
+        }
+    }
+
+    existsInTree(root, value) {
+        if (!root) {
+            return false;
+        } else {
+            let inLeft = this.existsInTree(root.left, value);
+            let inRight = this.existsInTree(root.right, value);
+            return root.value === value || inLeft || inRight;
+        }
+    }
+
+    reverseTree(root) {
+        if (!root) {
+            return;
+        } else {
+            this.reverseTree(root.left);
+            this.reverseTree(root.right);
+            [root.left, root.right] = [root.right, root.left];
+        }
+    }
+
+    isValidBST() {
+        return this.isValidHelper(this.root, -Infinity, Infinity)
+    }
+
+    isValidHelper(root, lower, upper) {
+        if (!root) return true
+        if ((root.value > lower) && (root.value < upper)) {
+            return this.isValidHelper(root.left, lower, root.value) && this.isValidHelper(root.right, root.value, upper)
+        } else {
+            return false
+        }
+    }
 }
 
 const bst = new BinarySearchTree();
@@ -166,16 +229,29 @@ bst.insert(15)
 bst.insert(3)
 bst.insert(7)
 
-console.log(bst.search(bst.root, 10))
-console.log(bst.search(bst.root, 5))
-console.log(bst.search(bst.root, 15))
-console.log(bst.search(bst.root, 20))
+console.log(bst.sumOfAllElements(bst.root))
+console.log(bst.maxValue(bst.root))
+console.log(bst.heightOfTree(bst.root))
+console.log(bst.existsInTree(bst.root, 3))
 
-bst.postOrder(bst.root)
-console.log(bst.min(bst.root))
-console.log(bst.max(bst.root))
+// console.log(bst.search(bst.root, 10))
+// console.log(bst.search(bst.root, 5))
+// console.log(bst.search(bst.root, 15))
+// console.log(bst.search(bst.root, 20))
+
+// bst.postOrder(bst.root)
+// console.log(bst.min(bst.root))
+// console.log(bst.max(bst.root))
 
 bst.levelOrder()
-bst.delete(10)
 console.log('----------------------------------------------------------------')
+
+// bst.reverseTree(bst.root)
+console.log('after reverseTree')
 bst.levelOrder()
+
+// bst.delete(10)
+console.log('----------------------------------------------------------------')
+console.log(bst.isValidBST())
+
+// bst.levelOrder()
