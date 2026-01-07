b483726c9012fde1462ab4a7bc6b2ee5e6fe05e9
muhsin <muhsin@zoptal.in>
2026-01-07 16:55:05 +0530

    Fix formatting in factorial and fibonacci console logs

--- algo/factorial.js
+++ algo/factorial.js
@@ -6,7 +6,7 @@ function factorial(n) {
     return res;
 }
 
-console.log(factorial(0)) // 1
-console.log(factorial(1)) // 1
-console.log(factorial(5)) // 120
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(5)); // 120
 // Big-0 = 0 (n)
--- algo/fibonacci.js
+++ algo/fibonacci.js
@@ -6,6 +6,6 @@ function fibonacci(n) {
     return fib;
 }
 
-console.log(fibonacci(2)) // [0,1]
-console.log(fibonacci(3)) // [0,1,1]
-console.log(fibonacci(7)) // [0, 1, 1, 2,3, 5, 8]
+console.log(fibonacci(2)); // [0, 1]
+console.log(fibonacci(3)); // [0, 1, 1]
+console.log(fibonacci(7)); // [0, 1, 1, 2, 3, 5, 8]
